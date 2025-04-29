c97aef6985532bea19c375db29ee937e8ab29af3
Samuel Moelius <sam@moeli.us>
2025-04-29 03:51:01 -0400

    Factor out `copy_into_tempdir` utility function from `globstar` test

--- tests/globstar.rs
+++ tests/globstar.rs
@@ -1,13 +1,11 @@
-use anyhow::{Result, ensure};
 use assert_cmd::{assert::OutputAssertExt, cargo::CommandCargoExt};
-use std::{fs::read_to_string, path::Path, process::Command};
-use tempfile::tempdir;
+use std::{fs::read_to_string, process::Command};
+
+mod util;
 
 #[test]
 fn globstar() {
-    let tempdir = tempdir().unwrap();
-    copy_into("fixtures/globstar", &tempdir).unwrap();
-    let path = tempdir.path().join("globstar");
+    let (_tempdir, path) = util::copy_into_tempdir("fixtures/globstar").unwrap();
 
     let mut command = Command::cargo_bin("rustdoc-prettier").unwrap();
     command.arg("**/*.rs");
@@ -16,13 +14,4 @@ fn globstar() {
 
     let contents = read_to_string(path.join("src/needs_formatting/mod.rs")).unwrap();
     assert_eq!("//! Needs formatting\n", contents);
-}
-
-fn copy_into(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<()> {
-    let mut command = Command::new("cp");
-    command.arg("-r");
-    command.args([from.as_ref(), to.as_ref()]);
-    let status = command.status()?;
-    ensure!(status.success(), "command failed: {command:?}");
-    Ok(())
 }
