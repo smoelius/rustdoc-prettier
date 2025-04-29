0c8bd9bb4e0fc5595953a82c033c15418062ce4c
Samuel Moelius <sam@moeli.us>
2025-04-29 03:52:52 -0400

    Add `markdown_link_check` and `udeps` tests to ci.rs

--- tests/ci.rs
+++ tests/ci.rs
@@ -1,4 +1,5 @@
 use assert_cmd::Command;
+use tempfile::tempdir;
 
 #[test]
 fn clippy() {
@@ -22,6 +23,33 @@ fn dylint() {
     Command::new("cargo")
         .args(["dylint", "--all", "--", "--all-targets"])
         .env("DYLINT_RUSTFLAGS", "--deny=warnings")
+        .assert()
+        .success();
+}
+
+#[test]
+fn markdown_link_check() {
+    let tempdir = tempdir().unwrap();
+
+    Command::new("npm")
+        .args(["install", "markdown-link-check"])
+        .current_dir(&tempdir)
+        .assert()
+        .success();
+
+    let readme_md = concat!(env!("CARGO_MANIFEST_DIR"), "/README.md");
+
+    Command::new("npx")
+        .args(["markdown-link-check", readme_md])
+        .current_dir(&tempdir)
+        .assert()
+        .success();
+}
+
+#[test]
+fn udeps() {
+    Command::new("cargo")
+        .args(["+nightly", "udeps", "--all-features", "--all-targets"])
         .assert()
         .success();
 }
