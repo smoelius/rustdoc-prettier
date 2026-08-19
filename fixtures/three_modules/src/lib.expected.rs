18b7b39d08ba104bbeb3591858c3fd2ec10df25e
Samuel Moelius <sam@moeli.us>
2026-08-19 04:32:07 -0400

    Work around off-by-one error in `rustfmt` `comment_width` implementation

--- fixtures/three_modules/src/lib.expected.rs
+++ fixtures/three_modules/src/lib.expected.rs
@@ -2,7 +2,8 @@
 /// and is so long that at the third level of indentation, it requires three lines!
 mod foo {
     /// This rustdoc comment occurs three times in this file, at three different levels of
-    /// indentation, and is so long that at the third level of indentation, it requires three lines!
+    /// indentation, and is so long that at the third level of indentation, it requires three
+    /// lines!
     mod bar {
         /// This rustdoc comments occurs three times in this file, at three different levels of
         /// indentation, and is so long that at the third level of indentation, it requires three
